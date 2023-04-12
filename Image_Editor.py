f882afdcab3322fe53eba6e0bd22cb4d29117f66
joacy <joacymsilva@gmail.com>
2023-04-12 10:45:13 -0300

    Configuração Inicial

---
+++ Image_Editor.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title="Web Scrapper",
+  page_icon="🌐",
+  layout="wide"
+)
+
+st.markdown("<h1 style='text-align: center;'>Image Editor</h1>", unsafe_allow_html=True)
